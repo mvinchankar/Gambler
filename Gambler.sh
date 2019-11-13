692c212edb207625c41f1f744aec83404ac84fa4
mvinchankar <mvinchankar18@gmail.com>
2019-11-13 12:43:15 +0530

    Program to know days won and lost and by how much

--- Gambler.sh
+++ Gambler.sh
@@ -2,9 +2,16 @@
 echo Welcome Gambler
 read -p "How Many Days you want to play" numberDays
 totalAmount=0;
+stake=100;
 cash=100;
+dayWin=0;
+dayloss=0;
 maxLimit=0;
 lowLimit=0;
+totalInvestment=0;
+lossAmount=0;
+winAmount=0;
+
 function limitsOfStakes()
 {
  maxLimit=$(($cash + $cash/2 ))
@@ -35,13 +42,38 @@ function bet()
          fi  
       fi
     done
-    echo DayCash: $cash 
+    echo DayCash: $cash
+    if [ $cash == $maxLimit ]
+    then 
+        dayWin=$(($dayWin + 1))
+    else
+        dayLoss=$(($dayLoss + 1))
+    fi
+    echo DayWin:$dayWin  DayLoss:$dayLoss 
     totalAmount=$(($totalAmount+$cash))
     echo Day limit Reached
   
  done
+ 
  echo $totalAmount
 }
+
+function totalInvestmentForDays()
+{
+echo Cash: $stake  Days :$numberDays
+totalInvestment=$(($stake*$numberDays))
+echo TotalInvestment:$totalInvestment
+if [ $totalAmount -lt $totalInvestment ]
+then
+    lossAmount=$(($totalInvestment-$totalAmount))
+    echo Loss:$lossAmount
+else
+    winAmount=$(($totalAmount-$totalInvestment))
+    echo Win:$winAmount
+fi
+}
+
 limitsOfStakes
 bet
+totalInvestmentForDays
 
