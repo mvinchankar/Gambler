9aa33d2d4186d766aebb61ec5fb60223cfe7efb7
mvinchankar <mvinchankar18@gmail.com>
2019-11-13 10:18:47 +0530

    FIrst case to bet every game as

--- Gambler.sh
+++ Gambler.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -x
 echo Welcome Gambler
+read -p "How Many Days you want to play" numberDays
+function bet()
+{
+
+for(( j=1; j<=numberDays; j++ ))
+do 
+  #read -p "How many times you want to play :" playTimes
+  cash=100;
+  while [ $cash != 0 ] 
+  do 
+    
+    cash=$(( $cash - 1 )) 
+    echo $cash
+  done
+done
+}
+bet
