042583a91b5ff4e0ba54741276dfde65a09125e6
mvinchankar <mvinchankar18@gmail.com>
2019-11-13 11:30:12 +0530

    Program to calculate the gambling total amount for 20 days

--- Gambler.sh
+++ Gambler.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 echo Welcome Gambler
 read -p "How Many Days you want to play" numberDays
+totalAmount=0;
 function bet()
 {
 
 for(( j=1; j<=numberDays; j++ ))
 do 
-  echo Day$numberDays
+  
   #read -p "How many times you want to play :" playTimes
   cash=100;
   while [ $cash != 0 ] 
@@ -15,21 +16,26 @@ do
     if [ $bet == 1 ]
     then 
         cash=$(( $cash + 1 )) 
-        echo You Win $cash
+       # echo You Win $cash
         if [ $cash == 150 ]
         then 
             break
         fi
     else
         cash=$(( $cash - 1 ))
-        echo You Lose $cash
+       # echo You Lose $cash
         if [ $cash == 50 ]
         then 
             break
         fi  
     fi
   done
-echo Day limit Reached
+  echo DayCash: $cash 
+  totalAmount=$(($totalAmount+$cash))
+  echo Day limit Reached
+  
 done
+echo $totalAmount
 }
 bet
+
