16e6ef1a84908a124a592557809fde52314baff8
mvinchankar <mvinchankar18@gmail.com>
2019-11-13 11:07:36 +0530

    Program to calculate whether the person win or lose 50% stake would resign for the day

--- Gambler.sh
+++ Gambler.sh
@@ -6,6 +6,7 @@ function bet()
 
 for(( j=1; j<=numberDays; j++ ))
 do 
+  echo Day$numberDays
   #read -p "How many times you want to play :" playTimes
   cash=100;
   while [ $cash != 0 ] 
@@ -15,11 +16,20 @@ do
     then 
         cash=$(( $cash + 1 )) 
         echo You Win $cash
+        if [ $cash == 150 ]
+        then 
+            break
+        fi
     else
         cash=$(( $cash - 1 ))
         echo You Lose $cash
+        if [ $cash == 50 ]
+        then 
+            break
+        fi  
     fi
   done
+echo Day limit Reached
 done
 }
 bet
